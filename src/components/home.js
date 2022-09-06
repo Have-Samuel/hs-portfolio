fffebfdfeb7e5fe06c8acfb2693eba236848d4ae
Have Samuel <samiehave9@gmail.com>
2022-09-07 02:29:38 +0300

    Created a home component

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className='home'>
+      <img src="home-img.png" className="home_img"></img>
+      <div className="text">
+        <span className="home-grt">Hello, my name is Have Samuel.</span>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
